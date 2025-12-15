75ecfb284b2d74013584337e21816547a7e4ae2c
lorenzo.begnozzi <lorenzo.begnozzi@coopertec.com.br>
2025-12-15 09:20:20 -0300

    aula 10

--- Aula 10 - Principais Tipo de JOINS/aula 10.sql
+++ Aula 10 - Principais Tipo de JOINS/aula 10.sql
@@ -1,5 +1,5 @@
 --Principais Tipo de JOINS
---INNER JOIN
+--INNER JOIN --> ou JOIN --> tras oq tem de igual ambas as tabelas
 SELECT
     pf.nr_sequencia,
     pf.nm_pessoa_fisica,
@@ -11,7 +11,7 @@ join pessoa_fisica_endereco pfe on(pf.nr_sequencia = pfe.nr_seq_pessoa_fisica)
 join municipio m on(m.nr_sequencia = pfe.nr_seq_municipio)
 join uf on(uf.nr_sequencia = pfe.nr_seq_uf);
 
---LEFT JOIN
+--LEFT JOIN --> (exemplo: se tiver endereço traz, caso n tenha, traz tambem)
 SELECT
     pf.nr_sequencia,
     pf.nm_pessoa_fisica,
@@ -23,4 +23,18 @@ left join pessoa_fisica_endereco pfe on(pf.nr_sequencia = pfe.nr_seq_pessoa_fisi
 left join municipio m on(m.nr_sequencia = pfe.nr_seq_municipio)
 left join uf on(uf.nr_sequencia = pfe.nr_seq_uf);
 
-commit;
+
+select 
+    pf.nr_sequencia,
+    pf.nm_pessoa_fisica,
+    pf.dt_nascimento,
+    m.ds_municipio,
+    uf.ds_uf
+from pessoa_fisica pf
+join pessoa_fisica_endereco pfe on (pf.nr_sequencia = pfe.nr_seq_pessoa_fisica)
+join municipio m on (m.nr_sequencia = pfe.nr_seq_municipio)
+join uf on (uf.nr_sequencia = pfe.nr_seq_uf);
+
+
+
+
